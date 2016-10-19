73e31d973cf5710f86e8884720f3f729144b7e10
Seretos <arne.von.appen@web.de>
2016-10-19 19:59:38 +0200

    added a repository function to get all package dependencies for one project

--- Repository/ProjectPackageDependencyRepository.php
+++ Repository/ProjectPackageDependencyRepository.php
@@ -20,25 +20,45 @@ class ProjectPackageDependencyRepository extends EntityRepository
      * @param Project $project
      * @param ProjectPackageDependency[] $usedDependencies
      */
-    public function removeUnusedDependencies(Project $project, array $usedDependencies){
+    public function removeUnusedDependencies(Project $project, array $usedDependencies)
+    {
         $query = $this->getEntityManager()
             ->createQueryBuilder()
             ->select('ppd')
-            ->from(ProjectPackageDependency::class,'ppd')
-            ->innerJoin(ProjectPackage::class,'pp','WITH','pp.id = ppd.sourceProjectPackage')
+            ->from(ProjectPackageDependency::class, 'ppd')
+            ->innerJoin(ProjectPackage::class, 'pp', 'WITH', 'pp.id = ppd.sourceProjectPackage')
             ->where('pp.project = :project')
             ->andWhere('ppd.id NOT IN(:dependencies)')
             ->getQuery();
 
-        $query->setParameter('project',$project);
-        $query->setParameter('dependencies',$usedDependencies);
+        $query->setParameter('project', $project);
+        $query->setParameter('dependencies', $usedDependencies);
 
         $deleteQuery = $this->getEntityManager()
             ->createQueryBuilder()
-            ->delete(ProjectPackageDependency::class,'ppd')
+            ->delete(ProjectPackageDependency::class, 'ppd')
             ->where('ppd.id IN(:unusedDependencies)')
             ->getQuery();
-        $deleteQuery->setParameter('unusedDependencies',$query->getResult());
+        $deleteQuery->setParameter('unusedDependencies', $query->getResult());
         $deleteQuery->execute();
+    }
+
+    /**
+     * @param Project $project
+     * @return ProjectPackageDependency[]
+     */
+    public function findByProject(Project $project)
+    {
+        $query = $this->getEntityManager()
+            ->createQueryBuilder()
+            ->select('ppd')
+            ->from(ProjectPackageDependency::class, 'ppd')
+            ->innerJoin(ProjectPackage::class, 'pp', 'WITH', 'pp.id = ppd.sourceProjectPackage')
+            ->where('pp.project = :project')
+            ->getQuery();
+
+        $query->setParameter('project', $project);
+
+        return $query->getResult();
     }
 }
--- Tests/Repository/ProjectPackageDependencyRepositoryTest.php
+++ Tests/Repository/ProjectPackageDependencyRepositoryTest.php
@@ -30,13 +30,14 @@ class ProjectPackageDependencyRepositoryTest extends PHPUnit_Framework_TestCase
         parent::setUp();
         $this->mockManager = $this->getMockBuilder(EntityManager::class)->disableOriginalConstructor()->getMock();
         $mockMetaData = $this->getMockBuilder(ClassMetadata::class)->disableOriginalConstructor()->getMock();
-        $this->repository = new ProjectPackageDependencyRepository($this->mockManager,$mockMetaData);
+        $this->repository = new ProjectPackageDependencyRepository($this->mockManager, $mockMetaData);
     }
 
     /**
      * @test
      */
-    public function removeUnusedDependencies(){
+    public function removeUnusedDependencies()
+    {
         $mockProject = $this->getMockBuilder(Project::class)->disableOriginalConstructor()->getMock();
         $dependencies = [];
         $dependencies[] = $this->getMockBuilder(ProjectPackageDependency::class)->disableOriginalConstructor()->getMock();
@@ -45,30 +46,54 @@ class ProjectPackageDependencyRepositoryTest extends PHPUnit_Framework_TestCase
         $this->mockManager->expects($this->at(0))->method('createQueryBuilder')->will($this->returnValue($mockSelectQueryBuilder));
 
         $mockSelectQueryBuilder->expects($this->at(0))->method('select')->with('ppd')->will($this->returnValue($mockSelectQueryBuilder));
-        $mockSelectQueryBuilder->expects($this->at(1))->method('from')->with(ProjectPackageDependency::class,'ppd')->will($this->returnValue($mockSelectQueryBuilder));
-        $mockSelectQueryBuilder->expects($this->at(2))->method('innerJoin')->with(ProjectPackage::class,'pp','WITH','pp.id = ppd.sourceProjectPackage')->will($this->returnValue($mockSelectQueryBuilder));
+        $mockSelectQueryBuilder->expects($this->at(1))->method('from')->with(ProjectPackageDependency::class, 'ppd')->will($this->returnValue($mockSelectQueryBuilder));
+        $mockSelectQueryBuilder->expects($this->at(2))->method('innerJoin')->with(ProjectPackage::class, 'pp', 'WITH', 'pp.id = ppd.sourceProjectPackage')->will($this->returnValue($mockSelectQueryBuilder));
         $mockSelectQueryBuilder->expects($this->at(3))->method('where')->with('pp.project = :project')->will($this->returnValue($mockSelectQueryBuilder));
         $mockSelectQueryBuilder->expects($this->at(4))->method('andWhere')->with('ppd.id NOT IN(:dependencies)')->will($this->returnValue($mockSelectQueryBuilder));
 
         $mockSelectQuery = $this->getMockBuilder(AbstractQuery::class)->disableOriginalConstructor()->getMock();
         $mockSelectQueryBuilder->expects($this->at(5))->method('getQuery')->will($this->returnValue($mockSelectQuery));
 
-        $mockSelectQuery->expects($this->at(0))->method('setParameter')->with('project',$mockProject)->will($this->returnValue($mockSelectQuery));
-        $mockSelectQuery->expects($this->at(1))->method('setParameter')->with('dependencies',$dependencies)->will($this->returnValue($mockSelectQuery));
+        $mockSelectQuery->expects($this->at(0))->method('setParameter')->with('project', $mockProject)->will($this->returnValue($mockSelectQuery));
+        $mockSelectQuery->expects($this->at(1))->method('setParameter')->with('dependencies', $dependencies)->will($this->returnValue($mockSelectQuery));
         $mockSelectQuery->expects($this->at(2))->method('getResult')->will($this->returnValue('unused'));
 
         $mockDeleteQueryBuilder = $this->getMockBuilder(QueryBuilder::class)->disableOriginalConstructor()->getMock();
         $this->mockManager->expects($this->at(1))->method('createQueryBuilder')->will($this->returnValue($mockDeleteQueryBuilder));
 
-        $mockDeleteQueryBuilder->expects($this->at(0))->method('delete')->with(ProjectPackageDependency::class,'ppd')->will($this->returnValue($mockDeleteQueryBuilder));
+        $mockDeleteQueryBuilder->expects($this->at(0))->method('delete')->with(ProjectPackageDependency::class, 'ppd')->will($this->returnValue($mockDeleteQueryBuilder));
         $mockDeleteQueryBuilder->expects($this->at(1))->method('where')->with('ppd.id IN(:unusedDependencies)')->will($this->returnValue($mockDeleteQueryBuilder));
 
         $mockDeleteQuery = $this->getMockBuilder(AbstractQuery::class)->disableOriginalConstructor()->getMock();
         $mockDeleteQueryBuilder->expects($this->at(2))->method('getQuery')->will($this->returnValue($mockDeleteQuery));
 
-        $mockDeleteQuery->expects($this->at(0))->method('setParameter')->with('unusedDependencies','unused')->will($this->returnValue($mockDeleteQuery));
+        $mockDeleteQuery->expects($this->at(0))->method('setParameter')->with('unusedDependencies', 'unused')->will($this->returnValue($mockDeleteQuery));
         $mockDeleteQuery->expects($this->at(1))->method('execute');
 
-        $this->repository->removeUnusedDependencies($mockProject,$dependencies);
+        $this->repository->removeUnusedDependencies($mockProject, $dependencies);
+    }
+
+    /**
+     * @test
+     */
+    public function findByProject()
+    {
+        $mockProject = $this->getMockBuilder(Project::class)->disableOriginalConstructor()->getMock();
+
+        $mockSelectQuery = $this->getMockBuilder(AbstractQuery::class)->disableOriginalConstructor()->getMock();
+        $mockSelectQuery->expects($this->at(0))->method('setParameter')->with('project', $mockProject)->will($this->returnValue($mockSelectQuery));
+        $mockSelectQuery->expects($this->at(1))->method('getResult')->will($this->returnValue('success'));
+
+        $mockSelectQueryBuilder = $this->getMockBuilder(QueryBuilder::class)->disableOriginalConstructor()->getMock();
+
+        $mockSelectQueryBuilder->expects($this->at(0))->method('select')->with('ppd')->will($this->returnValue($mockSelectQueryBuilder));
+        $mockSelectQueryBuilder->expects($this->at(1))->method('from')->with(ProjectPackageDependency::class, 'ppd')->will($this->returnValue($mockSelectQueryBuilder));
+        $mockSelectQueryBuilder->expects($this->at(2))->method('innerJoin')->with(ProjectPackage::class, 'pp', 'WITH', 'pp.id = ppd.sourceProjectPackage')->will($this->returnValue($mockSelectQueryBuilder));
+        $mockSelectQueryBuilder->expects($this->at(3))->method('where')->with('pp.project = :project')->will($this->returnValue($mockSelectQueryBuilder));
+        $mockSelectQueryBuilder->expects($this->at(4))->method('getQuery')->will($this->returnValue($mockSelectQuery));
+
+        $this->mockManager->expects($this->at(0))->method('createQueryBuilder')->will($this->returnValue($mockSelectQueryBuilder));
+
+        $this->assertSame('success', $this->repository->findByProject($mockProject));
     }
 }
